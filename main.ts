30bfdfb2ffce772e6e1b234e108877e7c8faec29
dylan0000 <58610746+dylan0000@users.noreply.github.com>
2020-02-05 13:50:18 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+
+
+
+    game.splash("true: take class " + "true: score 93% " + "action: A ")  
+
+
+    
+
+    game.splash("true: slip " + "false: fall " + "result: dont get hurt ")
+
+    game.splash("true: lever pulled " + "dont activate door ")
